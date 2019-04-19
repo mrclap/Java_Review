3fe70d0a51e7e360f41bf2abd221daf46b1014ed
clap <clap@kdml.co.kr>
2019-04-19 11:00:20 +0900

    ch02 on going

---
+++ Ch02/Literal.java
@@ -0,0 +1,13 @@
+class Literal {
+	public static void main(String[] args) {
+
+
+		// 1.
+		// 리터럴 3.14는 기본이 double 이므로 error
+		// float f = 3.14;
+
+		float f = 3.14f;
+		// or
+		double d = 3.14;
+	}
+}
